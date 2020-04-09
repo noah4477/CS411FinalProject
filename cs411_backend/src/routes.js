f726d8bcc4baf22a9f52e94d607534270e60ccbe
Noah Pell <noahpell@live.com>
2020-04-08 23:37:53 -0500

    Made Query to search Movies

--- cs411_backend/src/routes.js
+++ cs411_backend/src/routes.js
@@ -2,15 +2,39 @@ const { mysql, neo4j } = require('./db.js');
 
 module.exports = function(app) {
 
-    app.get("/api/test", function(req, res) {
-        return res.json({status: "OK", json_example_data: "DATA"});
-    });
+    //SELECT B.primaryName FROM (SELECT nconst FROM title_principals WHERE tconst = 'tt0133093') A INNER JOIN (SELECT primaryName, nconst FROM name_basics) B ON A.nconst = B.nconst
+    app.post("/api/search", function(req, res) {
 
-    app.get("/api/testmysql", function(req, res) {
+        var query = "";
+
+        if(req.body.type === "ALL")
+        {
+            query = `SELECT primarytitle, tconst, genres from title_basics WHERE primarytitle like "%${req.body.term}%" LIMIT ${return_limit}`;
+            mysql.query(query, function (err, result1) {
+                if (err) throw err;
+                query = `SELECT primaryName, nconst, knownForTitles from name_basics WHERE primaryName like "%${req.body.term}%" LIMIT ${return_limit}`;
+                mysql.query(query, function (err, result2) {
+                    if (err) throw err;
+                    return res.json({titles: result1, crew: result2});
+                  });
+            });
+        }
+        else if(req.body.type === "Actors") {
+            query = `SELECT primaryName, nconst, knownForTitles from name_basics WHERE primaryName like "%${req.body.term}%" AND 'actor' in primaryProfession LIMIT ${return_limit}`;
+        }
+        else if(req.body.type === "Movies") {
+            query = `SELECT primarytitle, tconst, genres  from title_basics WHERE primarytitle like "%${req.body.term}%" AND titleType ="movie" LIMIT ${return_limit}`;
+        }
+        else if(req.body.type === "TVShows") {
+            query = `SELECT primarytitle, tconst, genres  from title_basics WHERE primarytitle like "%${req.body.term}%" AND titleType in ('tvepisode', 'tvseries') LIMIT ${return_limit}`;
+        }
+        else if(req.body.type === "Directors") {
+            query = `SELECT primaryName, nconst, knownForTitles from name_basics WHERE primaryName like "%${req.body.term}%" AND 'director' in primaryProfession LIMIT ${return_limit}`;
+        }
         // SQL QUERY HERE
-        mysql.query("SHOW TABLES", function (err, result) {
+        mysql.query(query, function (err, result) {
             if (err) throw err;
             return res.json({result: result});
-          });
+        });
     });
 }
